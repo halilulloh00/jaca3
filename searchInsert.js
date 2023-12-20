9fdfc8cad1181d9ed397891226fff1ebd57647e8
Halilulloh <halilulloh@MacBook-Air-Halilulloh.local>
2023-12-20 19:37:21 +0600

    zadacha

---
+++ searchInsert.js
@@ -0,0 +1,9 @@
+// function ara(arr, nam) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] >= nam) {
+//             return i
+//         }
+//     }
+//     return arr.length
+// }
+// console.log(ara([1, 2, 3, 4], 2))
